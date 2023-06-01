04188d27a971042e79d6e00ae6e563292b4bdea2
Manuel Rivera <marp.developer@gmail.com>
2023-06-01 10:43:37 -0500

    promises and fetch class

--- scripts/load.js
+++ scripts/load.js
@@ -1,16 +1,38 @@
 const skillsDiv = document.getElementById("skills-container");
-const projectsDiv = document.getElementById("projects-container");
-console.log(db);
+const gallery = document.getElementById("gallery");
+
+var projectIndex = 0;
+function getCurrentProjects() {
+  return db.projects.slice(projectIndex, projectIndex + 3);
+}
 
 function render() {
-  console.log('Redender!!')
   skillsDiv.innerHTML = "";
   db.skills.forEach((skill) => {
     skillsDiv.innerHTML += `<button class="btn-common">${skill.name}</button>`;
   });
 
-  projectsDiv.innerHTML = "";
-  db.projects.forEach((project) => {
+  const projects = getCurrentProjects();
+  const firstProject = projects[0];
+
+  gallery.innerHTML = `
+      <div class="img-one d-flex-p-b">
+          <a class="btona">
+            <i class="fa-solid fa-arrow-up-right-from-square"></i>
+          </a>
+          <div class="section-project">
+            <h3>${firstProject.name}</h3>
+            <button class="btn-common">HTML</button>
+            <button class="btn-common">CSS</button>
+            <button class="btn-common">JAVASCRIPT</button>
+          </div>
+      </div>
+      <div class="half-width" id="projects-container"></div>
+  `;
+
+  const projectsDiv = document.getElementById("projects-container");
+  const secondZone = projects.slice(1, 3);
+  secondZone.forEach((project) => {
     projectsDiv.innerHTML += `
       <div class="full-height d-flex-p-b">
         <a class="btona">
@@ -23,10 +45,20 @@ function render() {
           <button class="btn-common">JAVASCRIPT</button>
         </div>
       </div>
-    `; 
+    `;
   });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+function nextProject() {
+  projectIndex++;
+  if (projectIndex > db.projects.length - 3) {
+    projectIndex = 0;
+  }
   render();
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  render();
+  setInterval(nextProject, 2000);
 });
---
+++ scripts/promises-fetch.js
@@ -0,0 +1,41 @@
+function getMiPromesa() {
+  const funct = (resolve, reject) => {
+    const confirmResult = 3;
+
+    if (confirmResult === 2) {
+      resolve("Este es el valor final");
+    } else {
+      reject("Se cancelo la operacion");
+    }
+  };
+
+  return new Promise(funct);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  // promise
+  console.log("primero");
+  miPromise = getMiPromesa();
+  miPromise
+    .then((valor) => {
+      console.log(valor);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  console.log(miPromise);
+  console.log("tercero");
+
+  // fetch
+  const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+  const responsePromise = fetch(url);
+
+  responsePromise
+    .then((result) => result.json())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
